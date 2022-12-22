40c24a85005d3bb1907450df29c5a97aae8c639e
Nikita <arzamaznik@gmail.com>
2022-12-22 17:10:24 +0600

    inference base class updated

--- src/demo_data.py
+++ src/demo_data.py
@@ -1,7 +1,7 @@
 import os
 import supervisely as sly
 
-demo_weights_url = "https://dl.fbaipublicfiles.com/detectron2/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl"
+demo_weights_url = "https://dl.fbaipublicfiles.com/detectron2/COCO-Detection/faster_rcnn_R_50_FPN_3x/137849458/model_final_280758.pkl"
 
 
 def prepare_weights():
